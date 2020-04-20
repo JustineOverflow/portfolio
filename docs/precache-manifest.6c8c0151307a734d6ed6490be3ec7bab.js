5a75cb5e06c066866a788137d5520eee8468c867
Justine Fabre <justinefabre84@gmail.com>
2020-04-20 20:38:54 +0100

    deployed version

---
+++ docs/precache-manifest.6c8c0151307a734d6ed6490be3ec7bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0841e87123b970cb9c029e129b18642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24aadd0165180779379",
+    "url": "/static/css/main.62db10c1.chunk.css"
+  },
+  {
+    "revision": "b5d38674111223f26ad7",
+    "url": "/static/js/2.2307a170.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2307a170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24aadd0165180779379",
+    "url": "/static/js/main.d738c033.chunk.js"
+  },
+  {
+    "revision": "79532a0fd89d9fc5e8c8",
+    "url": "/static/js/runtime-main.8f57da58.js"
+  }
+]);
